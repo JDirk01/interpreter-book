bdfa22ec8de6bd4396fec652212eaf20bea75e08
jarren <dirkj@oregonstate.edu>
2023-08-26 18:58:45 -0700

    lexer handles numbers, whitespace

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -18,6 +18,8 @@ func New(input string) *Lexer {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 		case '=':
 			tok = newToken(token.ASSIGN, l.ch)
@@ -42,6 +44,10 @@ func (l *Lexer) NextToken() token.Token {
 			if isLetter(l.ch) {
 				tok.Literal = l.readIdentifier()
 				tok.Type = token.LookupIdent(tok.Literal)
+				return tok
+			} else if isDigit(l.ch) {
+				tok.Type = token.INT
+				tok.Literal = l.readNumber()
 				return tok
 			} else {
 				tok = newToken(token.ILLEGAL, l.ch)
@@ -72,6 +78,24 @@ func (l *Lexer) readIdentifier() string {
 		l.readChar()
 	}
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func isLetter(ch byte) bool {
